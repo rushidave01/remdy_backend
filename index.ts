f060b7e4207c715c1e909ca9fbf910f512294ce6
Milan-26 <milanvaghasiya1126@gmail.com>
2025-05-09 21:10:00 +0530

    Fix(Allow-Cors): - Enabling cors for all routes

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
-import express, { Application, Request, Response } from 'express';
-import { connectDB } from './src/config/database';
-import { attachJourneyId } from './src/middleware/attachJourneyId';
-import { errorHandler } from './src/middleware/errorHandler';
-import { loggingMiddleware } from './src/middleware/loggingMiddleware';
-import { responseMiddleware } from './src/middleware/responseMiddleware';
-import { handleCors, rateLimiter, setSecurityHeaders } from './src/middleware/securityMiddleware';
-import routes from './src/routes';
-import logger from './src/utils/logger';
-import express_status_monitor from 'express-status-monitor';
-
-import dotenv from 'dotenv';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import express_status_monitor from "express-status-monitor";
+import { connectDB } from "./src/config/database";
+import { attachJourneyId } from "./src/middleware/attachJourneyId";
+import { errorHandler } from "./src/middleware/errorHandler";
+import { loggingMiddleware } from "./src/middleware/loggingMiddleware";
+import { responseMiddleware } from "./src/middleware/responseMiddleware";
+import {
+  rateLimiter,
+  setSecurityHeaders,
+} from "./src/middleware/securityMiddleware";
+import routes from "./src/routes";
+import logger from "./src/utils/logger";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app: Application = express();
@@ -18,10 +21,10 @@ const app: Application = express();
 app.use(express_status_monitor());
 
 // Middleware setup
-app.use(express.json( { limit: '50mb' } ));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(setSecurityHeaders); // Security headers
-app.use(handleCors); // CORS
+app.use(cors()); // CORS
 // app.use(handleCsrfProtection); // CSRF protection
 app.use(rateLimiter);
 app.use(attachJourneyId);
@@ -42,15 +45,14 @@ connectDB()
   });
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('API is running...');
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running...");
 });
 
 // Mount API routes
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 // Error handling middleware
 app.use(errorHandler);
 
 export default app;
- 
